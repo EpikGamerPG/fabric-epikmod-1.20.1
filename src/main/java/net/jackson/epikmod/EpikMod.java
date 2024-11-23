9135b3c6af884c3646f4306f47e4252598b66148
Pg Jackson <epikgamerpg@gmail.com>
2024-11-23 19:33:52 +0100

    He said its very important

---
+++ src/main/java/net/jackson/epikmod/EpikMod.java
@@ -0,0 +1,17 @@
+package net.jackson.epikmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EpikMod implements ModInitializer {
+	public static final String MOD_ID = "epikmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+
+	}
+}
---
+++ src/main/java/net/jackson/epikmod/EpikModClient.java
@@ -0,0 +1,11 @@
+package net.jackson.epikmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EpikModClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
